43512b8d06976f5819c1995d45dcaa94e1f4b20e
iambin6 <miraisan2512@gmail.com>
2025-03-04 09:42:22 +0700

    Add files via upload

---
+++ bai2/Manager.java
@@ -0,0 +1,131 @@
+package week6.bai2;
+
+import week6.bai2.History;
+import week6.bai2.Worker;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+
+public class Manager {
+
+
+    public static int menu() {
+        System.out.println("1. Add worker.");
+        System.out.println("2. Increase salary for worker.");
+        System.out.println("3. Decrease for worker");
+        System.out.println("4. Show adjusted salary worker information");
+        System.out.println("5. Exist");
+        System.out.print("Enter your choice: ");
+        int choice = Validate.checkInputIntLimit(1, 5);
+        return choice;
+    }
+
+    public static void addWorker(ArrayList<Worker> lw) {
+        System.out.print("Enter code: ");
+        String id = Validate.checkInputString();
+        System.out.print("Enter name: ");
+        String name = Validate.checkInputString();
+        System.out.print("Enter age: ");
+        int age = Validate.checkInputIntLimit(18, 50);
+        System.out.print("Enter salary: ");
+        int salary = Validate.checkInputSalary();
+        System.out.print("Enter work location: ");
+        String workLocation = Validate.checkInputString();
+        if (!Validate.checkWorkerExist(lw, id, name, age, salary, workLocation)) {
+            System.err.println("Duplicate.");
+        } else {
+            lw.add(new Worker(id, name, age, salary, workLocation));
+            System.err.println("Add success.");
+        }
+    }
+
+    public static void changeSalary(ArrayList<Worker> lw, ArrayList<History> lh, int status) {
+        if (lw.isEmpty()) {
+            System.err.println("List empty.");
+            return;
+        }
+        System.out.print("Enter code: ");
+        String id = Validate.checkInputString();
+        Worker worker = getWorkerByCode(lw, id);
+        if (worker == null) {
+            System.err.println("Not exist worker.");
+        } else {
+            int salaryCurrent = worker.getSalary();
+            int salaryUpdate;
+            //kiểm tra lương thay đổi
+            if (status == 1) {
+                System.out.print("Enter salary: ");
+                //lặp tới khi lương cập nhật > lương hiện tại
+                while (true) {     
+                    salaryUpdate = Validate.checkInputSalary();
+                    //kiểm tra đầu vào
+                    if (salaryUpdate <= salaryCurrent) {
+                        System.err.println("Must be greater than current salary.");
+                        System.out.print("Enter again: ");
+                    } else {
+                        break;
+                    }
+                }
+                lh.add(new History("UP", getCurrentDate(), worker.getId(),
+                        worker.getName(), worker.getAge(), salaryUpdate,
+                        worker.getWorkLocation()));
+            } else {
+                System.out.print("Enter salary: ");
+                //lặp tới khi lương cập nhật < lương hiện tại
+                while (true) {
+                    salaryUpdate = Validate.checkInputSalary();
+                    //kiểm tra đầu vào
+                    if (salaryUpdate >= salaryCurrent) {
+                        System.err.println("Must be smaller than current salary.");
+                        System.out.print("Enter again: ");
+                    } else {
+                        break;
+                    }
+                }
+                lh.add(new History("DOWN", getCurrentDate(), worker.getId(),
+                        worker.getName(), worker.getAge(), salaryUpdate,
+                        worker.getWorkLocation()));
+            }
+            worker.setSalary(salaryUpdate);
+            System.err.println("Update success");
+        }
+    }
+
+    public static void printListHistory(ArrayList<History> lh) {
+        if (lh.isEmpty()) {
+            System.err.println("List empty.");
+            return;
+        }
+        System.out.printf("%-5s%-15s%-5s%-10s%-10s%-20s\n", "Code", "Name", "Age",
+                "Salary", "Status", "Date");
+        Collections.sort(lh);
+        for (History history : lh) {
+            printHistory(history);
+        }
+    }
+
+    //lấy worker
+    public static Worker getWorkerByCode(ArrayList<Worker> lw, String id) {
+        for (Worker worker : lw) {
+            if (id.equalsIgnoreCase(worker.getId())) {
+                return worker;
+            }
+        }
+        return null;
+    }
+
+    //ngày hiện tại
+    public static String getCurrentDate() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar calendar = Calendar.getInstance();
+        return dateFormat.format(calendar.getTime());
+    }
+
+    public static void printHistory(History history) {
+        System.out.printf("%-5s%-15s%-5d%-10d%-10s%-20s\n", history.getId(),
+                history.getName(), history.getAge(), history.getSalary(),
+                history.getStatus(), history.getDate());
+    }
+}
